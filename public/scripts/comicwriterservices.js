326a787faf003d0caee0a661c5d638e81880285c
= <=>
2021-03-09 07:57:58 +0000

    Fixed last remaing bugs. All done!

--- public/scripts/comicwriterservices.js
+++ public/scripts/comicwriterservices.js
@@ -50,6 +50,8 @@ const app = new Vue({
         addFilterToArticles: function() {
             for(let articleIdx=0; articleIdx < this.articles.length; articleIdx++) {
                 for(let linkIdx=0; linkIdx < this.articles[articleIdx].links.length; linkIdx++) {
+                    totalAuthors = this.articles[articleIdx].links[linkIdx].Authors.length - 1;
+
                     for(let authorIdx=0; authorIdx < this.articles[articleIdx].links[linkIdx].Authors.length; authorIdx++) {
                         for(let filterAuthorIndex=0; filterAuthorIndex < this.filteredAuthors.length; filterAuthorIndex++) {
                             if (this.filteredAuthors[filterAuthorIndex].fullname.trim() === this.articles[articleIdx].links[linkIdx].Authors[authorIdx].fullname.trim()) {
@@ -61,56 +63,10 @@ const app = new Vue({
                                     this.articles[articleIdx].links[linkIdx].filterCSS = "hidden";
                                 }
                             }
-                            
                         }
                     }
                 }
             }
-        },
-        debugLinks: function() {
-            for(let articleIdx=0; articleIdx < this.articles.length; articleIdx++) {
-                console.log("Articles Category: " + this.articles[articleIdx].category);
-                for(let linkIdx=0; linkIdx < this.articles[articleIdx].links.length; linkIdx++) {
-                    console.log("Link Title: " + this.articles[articleIdx].links[linkIdx].Title);
-                    //console.log("Link Authors Count: " + this.articles[articleIdx].links[linkIdx].Authors.length);
-                    //console.log("Link Author (0): " + this.articles[articleIdx].links[linkIdx].Authors[0].fullname);
-                    for(let authorIdx=0; authorIdx < this.articles[articleIdx].links[linkIdx].Authors.length; authorIdx++) {
-                        console.log("Author: " + this.articles[articleIdx].links[linkIdx].Authors[authorIdx].fullname);
-                    }
-                }
-            }
-        },
-        debugFilteredAuthors: function() {
-            for(let authorIdx = 0; authorIdx < this.filteredAuthors.length; authorIdx++) {
-                console.log("Author FirstName " + this.filteredAuthors[authorIdx].firstname);
-                console.log("Author LastName " + this.filteredAuthors[authorIdx].lastname);
-                console.log("Author Fullname " + this.filteredAuthors[authorIdx].fullname);
-            }
-        },
-        debugLinkObj: function(data) {
-            console.log("Link Titlte " + data.Title);
-            console.log("Link Authors");
-            for(let authorIdx = 0; authorIdx < data.Authors.length; authorIdx++) {
-                console.log("Author FirstName " + data.Authors[authorIdx].firstname);
-                console.log("Author LastName " + data.Authors[authorIdx].lastname);
-                console.log("Author Fullname " + data.Authors[authorIdx].fullname);
-                console.log("");
-            }
-        },
-        debugCurrentAuthor: function() {
-
-
-            for(let authorIdx = 0; authorIdx < this.filteredAuthors.length; authorIdx++) {
-                if (this.filteredAuthors[authorIdx].fullname === this.currentAuthorObj.fullname) {
-                    console.log("Current Author matches up");
-                    console.log("Author FirstName " + this.currentAuthorObj.firstname);
-                    console.log("Author LastName " + this.currentAuthorObj.lastname);
-                    console.log("Author Fullname " + this.currentAuthorObj.fullname);
-                }
-            }
-
-            this.debugFilteredAuthors();
-
         }
     }
 });
