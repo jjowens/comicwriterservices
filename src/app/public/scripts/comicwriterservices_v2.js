6194f56371951fa6cbe5e187dbc4186bbad45d40
John Owens <gbcritic@hotmail.com>
2026-03-25 05:39:55 +0000

    Search query (#18)
    * added sort feature
    
    * removed parts
    
    ---------
    
    Co-authored-by: = <=>

--- src/app/public/scripts/comicwriterservices_v2.js
+++ src/app/public/scripts/comicwriterservices_v2.js
@@ -6,7 +6,9 @@ const app = new Vue({
     data: {
         authors: [],
         articles: [],
-        debugPanelVisible: 'hidden'
+        debugPanelVisible: 'hidden',
+        sortPropName: "category_title",
+        sortPropOrder: "asc"
     },
     methods: {
         preload: function(event) {
@@ -19,7 +21,51 @@ const app = new Vue({
             this.articles = data.articles;
         },
         organiseMyArticles: function() {
-            return this.articles
+            if (this.sortPropOrder === "asc") {
+                return this.sortArticlesByProp(this.sortPropName)
+            } else if (this.sortPropOrder === "desc")  {
+                return this.sortArticlesByPropReverse(this.sortPropName)
+            } else {
+                return this.sortArticlesByProp(this.sortPropName)
+            }
+        },
+        sortArticlesByProp: function(propName) {
+            if (propName === 'title') {
+                return this.articles.slice().sort(function (a,b) {
+                    if (a.title < b.title) return -1;
+                    if (a.title > b.title) return 1;
+                    return 0;
+                })
+            } else if (propName === 'category_title') {
+                return this.articles.slice().sort(function (a,b) {
+                    return (a.category.localeCompare(b.category) || a.title.localeCompare(b.title))
+                })
+            } else {
+                return this.articles.slice().sort(function (a,b) {
+                    if (a.title < b.title) return -1;
+                    if (a.title > b.title) return 1;
+                    return 0;
+                })
+            }
+        },
+        sortArticlesByPropReverse: function(propName) {
+            if (propName === 'title') {
+                return this.articles.slice().sort(function (a,b) {
+                    if (a.title > b.title) return -1;
+                    if (a.title < b.title) return 1;
+                    return 0;
+                })
+            } else if (propName === 'category_title') {
+                return this.articles.slice().sort(function (a,b) {
+                    return (b.category.localeCompare(a.category) || a.title.localeCompare(b.title))
+                })
+            } else {
+                return this.articles.slice().sort(function (a,b) {
+                    if (a.title > b.title) return -1;
+                    if (a.title < b.title) return 1;
+                    return 0;
+                })
+            }
         }
     }
 });
